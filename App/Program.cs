5e45a567d235b04c971eda5273db11f52e306db4
lyonchikk1 <alena.karpushina2015@yandex.ru>
2022-10-04 20:10:51 +0300

    add Program.cs

--- App/Program.cs
+++ App/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите строки через зяпятую (пример: \"hello\",\"2\",\"world\",\":-)\")");
+string input = Console.ReadLine().TrimStart('"').TrimEnd('"');
+string[] mas = input.Split("\",\"");
+string[] result = Array.Empty<string>();
+
+foreach (string st in mas)
+{
+    if (st.Length <= 3)
+    {
+        result = result.Concat(new String[] { st }).ToArray();
+    }
+}
+
+Console.WriteLine($"[\"{input}\"] -> [\"{string.Join("\",\"", result)}\"]");
